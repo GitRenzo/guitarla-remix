afd904d8b4d0f1e29810077d4128b4935f527e35
GitRenzo <Renzoelm25@gmail.com>
2023-06-07 15:44:10 -0400

    Project updated

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,18 +1,37 @@
-export function loader(){
+import Post from "../components/post"
+import { getPosts } from "../model/posts.server"
+import { useLoaderData } from "@remix-run/react"
+import styles from "../styles/blog.css"
 
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles 
+    }
+  ]
+}
+
+export async function loader() {
+  const posts = await getPosts()
+  return posts.data
 }
 
 function Blog() {
+  const posts = useLoaderData()
   return (
-      <main className="contenedor">
-        <h2 className="heading">
-
-        </h2>
-        <div className="blog">
-
-        </div>
-      </main>
-    )
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {posts.map(post => (
+          <Post
+            key={post.id}
+            post={post.attributes}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default Blog
